432c20fae96b6a1b247d805cb0935c9970b2d278
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-29 21:14:09 +0600

    added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar></Navbar>
     </div>
   );
 };
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import NavbarLogo from '../../assets/navbarLogo.png'
+import CartIcon from '../../assets/cartIcon.png'
+
+const Navbar = () => {
+  return (
+    <header>
+    <nav className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-50 p-2 shadow text-[#101727]">
+        <li><a>Products</a></li>
+        <li>
+          <a>Features</a>
+          <ul className="p-2">
+            <li><a>AI-Powered Automation</a></li>
+            <li><a>Seamless Integration</a></li>
+            <li><a>Advanced Analytics</a></li>
+            <li><a>Security & Privacy</a></li>
+          </ul>
+        </li>
+        <li><a>Pricing</a></li>
+        <li><a>Testimonials</a></li>
+        <li><a>FAQ</a></li>
+      </ul>
+    </div>
+    <img className='w-32 h-auto' src={NavbarLogo} alt="" />
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 text-[#101727]">
+      <li><a>Products</a></li>
+      <li>
+        <details>
+          <summary>Features</summary>
+          <ul className="p-2 bg-base-100 w-48 z-1">
+            <li><a>AI-Powered Automation</a></li>
+            <li><a>Seamless Integration</a></li>
+            <li><a>Advanced Analytics</a></li>
+            <li><a>Security & Privacy</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Pricing</a></li>
+      <li><a>Testimonials</a></li>
+      <li><a>FAQ</a></li>
+    </ul>
+  </div>
+  
+  <div className="navbar-end gap-3">
+    <img className="cursor-pointer w-5 h-5" src={CartIcon} alt="" />
+    <a className="cursor-pointer">Login</a>
+    <a className="btn rounded-full bg-[linear-gradient(to_right,#4F39F6,#9514FA)]">Get Started</a>
+  </div>
+    </nav>
+    </header>
+  );
+};
+
+export default Navbar;
