8a2087ad85506fd08f84d896b17e0467e1f00f3c
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-01 19:09:07 +0600

    The p tag in the App.jsx file has been modified

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const [addCarts, setAddCarts] = useState([])
 <div className='text-center space-y-6 mb-10 mt-15 px-4'>
   <div className='max-w-2xl mx-auto space-y-1.5'>
     <h1 className='text-3xl md:text-[35px] font-bold'>Premium Digital Tools</h1>
-    <p className='text-gray-400'>Choose from our curated collection...</p>
+    <p className='text-gray-400'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
   </div>
 </div>
 
